35a27a7fb6020b0c8804c8ac0d91af3973766a28
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2021-04-11 20:16:43 +0200

    added working chart example with click events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+import React from 'react';
+
+const sdk = new ChartsEmbedSDK({
+  baseUrl: "https://charts.mongodb.com/charts-philip-eschenbacher-rrntx"
+});
+
+const chart1 = sdk.createChart({
+  chartId: "1e05231d-03de-4a11-a9c8-00d4de2dba2f",
+  height: "700px"
+});
+
+const chart2 = sdk.createChart({
+  chartId: "dff5f49e-87d4-4fc6-9ee0-6371408c506c",
+  height: "700px",
+  theme: "dark"
+});
+
+
+class MyChart extends React.Component {
+  async componentDidMount() {
+    await chart1.render(document.getElementById("mongochart1"));
+    await chart1.addEventListener("click", (payload) => {
+      console.log("CLICK");
+      if (payload.target.role === "mark") {
+        chart2.setFilter(payload.selectionFilter);
+      } else {
+        chart2.setFilter({});
+      }
+    });
+
+    await chart2.render(document.getElementById("mongochart2"));
+  }
+
+  render() {
+    return(
+      <div style={{display: "flex", justifyContent: "space-around", width: "100%"}}>
+        <div style={{minWidth: "700px"}} id="mongochart1" />
+        <div style={{minWidth: "700px"}} id="mongochart2" />
+      </div>
+    );
+  }
+} 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyChart />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,7 +11,55 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+
+// const sdk = new ChartsEmbedSDK({
+//   baseUrl: "https://charts.mongodb.com/charts-embedding-examples-wgffp", // Optional: ~REPLACE~ with the Base URL from your Embed Chart dialog
+// });
+
+// const chart = sdk.createChart({
+//   chartId: "90a8fe84-dd27-4d53-a3fc-0e40392685dd", // Optional: ~REPLACE~ with the Chart ID from your Embed Chart dialog
+//   height: "700px",
+// });
+
+// const clickHandler = (payload) => {
+//   document.getElementById("payload").innerHTML =
+//     "<pre>" + JSON.stringify(payload, null, 2) + "</pre>";
+
+//   let infoText = "";
+//   if (payload.target.role) {
+//     infoText += `<li>Role: ${payload.target.role}</li>`;
+//     infoText += `<li>Type: ${payload.target.type}</li>`;
+//   }
+
+//   if (payload.target.fill) {
+//     infoText += `<li>Fill: <span style="color:${payload.target.fill}">${payload.target.fill}</li>`;
+//   }
+//   if (payload.data.x) {
+//     infoText += `<li>x.label: ${payload.data.x.label}</li>`;
+//     infoText += `<li>x.value: ${payload.data.x.value}</li>`;
+//   }
+//   if (payload.data.y) {
+//     infoText += `<li>y.label: ${payload.data.y.label}</li>`;
+//     infoText += `<li>y.value: ${payload.data.y.value}</li>`;
+//   }
+//   if (payload.data.color) {
+//     infoText += `<li>color.label: ${payload.data.color.label}</li>`;
+//     infoText += `<li>color.value: ${payload.data.color.value}</li>`;
+//   }
+
+//   document.getElementById("info").innerHTML = "<ul>" + infoText + "</ul>";
+// };
+
+// async function renderCharts() {
+//   await chart.render(document.getElementById("root"));
+//   await chart.addEventListener("click", clickHandler);
+// }
+
+// renderCharts().catch((e) => window.alert(e.message));
